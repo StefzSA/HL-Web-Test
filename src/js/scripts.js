cadec06c09aef5bcaa1dfd3ed275a1095c6c2f1d
Stefano Strippoli <stefano.g.strippoli@gmail.com>
2024-10-11 12:26:21 -0400

    Added increasing number animations, and a basic animation framework on scroll.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,9 +3,40 @@ const banner = document.getElementById("banner");
 const closeBanner = document.querySelector(".banner_close");
 closeBanner.addEventListener("click", () => {
   let header = document.getElementById("header");
-  banner.style.display = 'none';
-  header.style.marginTop = '0'
+  banner.style.display = "none";
+  header.style.marginTop = "0";
 });
+
+function checkViewport(element) {
+  const rect = element.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  return rect.top >= 0 && rect.bottom <= windowHeight+20;
+}
+
+function increasingNumbers(el, target, interval = 2000, steps = 50) {
+  const element = el;
+  const targetValue = parseInt(target.replace(/[^0-9]/g, ""), 10);
+  const ratio = interval / steps;
+  let currentValue = 0;
+  const remaining = targetValue - currentValue;
+  const increment = remaining / ratio;
+  const symbol = element.textContent.replace(/[0-9,]/g, "");
+  const formatter = new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 0,
+    roundingMode: "trunc",
+    roundingPriority: "auto",
+  });
+
+  const animate = setInterval(() => {
+    currentValue += increment;
+    let formattedNumber = formatter.format(currentValue);
+    element.textContent =symbol == "%" || symbol == ""? Math.round(formattedNumber) + symbol : symbol + formattedNumber;
+    if (currentValue >= targetValue) {
+      clearInterval(animate);
+    }
+  }, 10);
+}
 
 const carousel = document.getElementById("testimonials");
 function startCarousel(carousel, interval) {
@@ -59,10 +90,8 @@ function createDots(carousel) {
     );
     return;
   }
-
   const items = carousel.querySelectorAll(".carousel-item").length;
 
-  // Create and insert a dot for each carousel item
   for (let i = 0; i < items; i++) {
     const dot = document.createElement("div");
     dot.classList.add("dot");
@@ -71,17 +100,17 @@ function createDots(carousel) {
   }
 
   carousel.querySelector(".dot").classList.add("active");
-  carousel.querySelector(".dot").setAttribute("disabled", true); 
+  carousel.querySelector(".dot").setAttribute("disabled", true);
 }
 
 function assignData(carousel) {
   let i = 0;
-  const carousels = document.querySelectorAll(".carousel"); 
+  const carousels = document.querySelectorAll(".carousel");
 
   carousels.forEach((carouselElement) => {
     carouselElement.dataset.carouselid = i++;
   });
-  i=0;
+  i = 0;
   carousel.querySelectorAll(".carousel-item").forEach((item) => {
     item.dataset.slideid = i++;
   });
@@ -103,19 +132,15 @@ function dotsNavigation(carousel, interval) {
         );
 
         currentSlide.classList.remove("active");
-        currentSlide.style.opacity = 0; 
+        currentSlide.style.opacity = 0;
 
         nextSlide.classList.add("active");
-        nextSlide.style.opacity = 1; 
+        nextSlide.style.opacity = 1;
 
-        
         currentDot.classList.remove("active");
         currentDot.removeAttribute("disabled");
         dot.classList.add("active");
         dot.setAttribute("disabled", true);
-
-        carousel.classList.add("paused");
-        startAutoplay(carousel, interval);
       }
     });
   });
@@ -123,8 +148,10 @@ function dotsNavigation(carousel, interval) {
 
 function nextSlide(carousel) {
   const currentSlide = carousel.querySelector(".carousel-item.active");
-  const nextSlide = (currentSlide.nextElementSibling) ?  currentSlide.nextElementSibling : carousel.querySelector(".carousel-item");
-    
+  const nextSlide = currentSlide.nextElementSibling
+    ? currentSlide.nextElementSibling
+    : carousel.querySelector(".carousel-item");
+
   currentSlide.classList.remove("active");
   currentSlide.style.opacity = 0;
 
@@ -134,20 +161,50 @@ function nextSlide(carousel) {
 }
 
 function updateDots(carousel) {
-    const dots = carousel.querySelectorAll(".dot");
-  
-    dots.forEach((dot) => {
-      dot.classList.remove("active");
-      dot.removeAttribute("disabled");
-    });
-  
-    const activeSlide = carousel.querySelector(".carousel-item.active");
-    const activeDotIndex = activeSlide.dataset.slideid;
-    const activeDot = dots[activeDotIndex];
-  
-    activeDot.classList.add("active");
-    activeDot.setAttribute("disabled", true);
-  }
+  const dots = carousel.querySelectorAll(".dot");
 
-  let testimonials = document.getElementById('testimonials');
-        startCarousel(testimonials, 3000);
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+    dot.removeAttribute("disabled");
+  });
+
+  const activeSlide = carousel.querySelector(".carousel-item.active");
+  const activeDotIndex = activeSlide.dataset.slideid;
+  const activeDot = dots[activeDotIndex];
+
+  activeDot.classList.add("active");
+  activeDot.setAttribute("disabled", true);
+}
+
+let numbers = document.querySelectorAll(".fund_misc span");
+numbers.forEach((el) => {
+  if (checkViewport(el)) {
+    if (!el.classList.contains("increasing")) {
+      increasingNumbers(el, el.textContent, 5000, 25);
+      el.classList.add("increasing");
+    }
+  }
+  window.addEventListener("scroll", () => {
+    if (checkViewport(el)) {
+      if (!el.classList.contains("increasing")) {
+        increasingNumbers(el, el.textContent, 5000, 25);
+        el.classList.add("increasing");
+      }
+    }
+  });
+});
+
+let testimonials = document.getElementById("testimonials");
+startCarousel(testimonials, 3000);
+
+const animated = document.querySelectorAll(".FadeInUp, .FadeInDown, .FadeInLeft, .FadeInRight");
+animated.forEach((element) => {
+  if (checkViewport(element)) {
+    element.classList.add("animated");
+  }
+  window.addEventListener("scroll", () => {
+    if (checkViewport(element)) {
+      element.classList.add("animated");
+    }
+  });
+});
